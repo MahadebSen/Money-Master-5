d3e4ff1721f0fafee5c60f872a11817ecc7194ac
Mahadeb Sen <mahadebsen237@gmail.com>
2022-02-18 07:31:52 +0530

    function added

--- JS file/script.js
+++ JS file/script.js
@@ -1,33 +1,70 @@
 
+function getNumberValue(inputField){
+    // debugger;
+    const inpueText = document.getElementById(inputField);
+    const inputNumber = parseInt(inpueText.value);
+    
+    if (/*inputNumber != "string" ||*/ inputNumber < 0){
+        return alert("Please Enter a Possitive Number");
+    }
+    return inputNumber;
+};
+
+
+
+
 // calculate button
-document.getElementById('calculate').addEventListener('click', function(){
-    const income = document.getElementById('income').value;
-    const food = document.getElementById('food').value;
-    const rent = document.getElementById('rent').value;
-    const clothes = document.getElementById('clothes').value;
+document.getElementById('calculate').addEventListener('click', function(event){
+    const income = getNumberValue('income');
+    const food = getNumberValue('food');
+    const rent = getNumberValue('rent');
+    const clothes = getNumberValue('clothes');
+    
 
     // total Expenses
     const totalEx = document.getElementById('total-ex');
-    totalEx.innerText = parseInt(food) + parseInt(rent) + parseInt(clothes);
+    totalEx.innerText = food + rent + clothes;
 
     // balance
     const balance = document.getElementById('balance');
-    balance.innerText = parseInt(income) - parseInt(totalEx.innerText);
+    balance.innerText = income - parseInt(totalEx.innerText);
     
-});
 
+});
+ 
 // save
 document.getElementById('save-btn').addEventListener('click', function(){
+
     const savingAmount = document.getElementById('saving-amount');
-    const saveInput = document.getElementById('save-input');
+    const saveInput = getNumberValue('save-input');
     const remainingBalance = document.getElementById('remaining-balance');
 
     // saving amount
-    savingAmount.innerText = parseInt(income.value) *  parseInt(saveInput.value) / 100;
+    savingAmount.innerText = income.value * saveInput / 100;
 
     // remaining
     remainingBalance.innerText = parseInt(balance.innerText) - parseInt(savingAmount.innerText);
 
 });
 
+// clear 
+document.getElementById('clear').addEventListener('click', function(){
+    debugger
+    income.value = '';
+    food.value = '';
+    rent.value = '';
+    clothes.value = '';
+    document.getElementById('save-input').value = "";
+
+    /*
+    totalEx.innerText = '';
+    balance.innerText = '';
+    savingAmount.innerText = '';
+    remainingBalance.innerText = '';*/
+
+    document.getElementById('total-ex').innerText = 0;
+    balance.innerText = 0;
+    document.getElementById('saving-amount').innerText = 0;
+    document.getElementById('remaining-balance').innerText = 0;
+})
 
